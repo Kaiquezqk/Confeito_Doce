8445b04ea6d932558ab9a2229a187a544641939f
KAIQUE LOPES ROMUALDO <47425510871@imaccommand037.local>
2025-12-12 14:49:32 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,46 +1,16 @@
-//Menu de Hamburguer
-let menu = document.getElementById("menu")
-let iconeBarras = document.getElementById("icone-barras")
-let iconeX = document.getElementById("icone-x")
-let body = document.getElementsByTagName("body")
-let sombra = document.getElementById("sombra")
+// script.js
 
+document.addEventListener("DOMContentLoaded", () => {
+  const fileiras = document.querySelectorAll(".fileira"); 
+  fileiras.forEach(fileira => {
+    const imagens = Array.from(fileira.querySelectorAll("img"));
+    let index = 0;
 
-function abreFechaMenu() {
-    //Se o menu está fechado
-    if (window.getComputedStyle(menu).right == "-210px") {
-        //Abrir menu
-        menu.style.right = "0"
-        
-        //Mostrar icone x
-        iconeX.style.display = "inline"
-        
-        //Esconder icone barras 
-        iconeBarras.style.display = "none"
-        
-        sombra.style.right = "0"
-    } else {
-        //Fechar o menu
-        menu.style.right = "-210px"
-        
-        // Esconder icone X
-        iconeX.style.display = "none"
-        
-        // Mostrar icone barras
-        iconeBarras.style.display = "inline"
-        
-        sombra.style.right = "-101vw"
-    }
-}
-
-onresize = () => {
-    if (window.getComputedStyle(menu).right == "-210px") {
-        // Mostrar icone X
-        iconeX.style.display = "inline"
-    }
-    else{
-        // Esconder icone barras
-        iconeBarras.style.display = "none"
-    }
-}
-
+    setInterval(() => {
+      
+      const primeira = imagens.shift();
+      fileira.appendChild(primeira);
+      imagens.push(primeira);
+    }, 2000); 
+  });
+});
